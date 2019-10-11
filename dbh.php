2204ca235af17852ce6c8a654363ad73cb8cb780
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-10-11 12:04:56 -0400

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,8 @@
+<?
+
+
+$dbh=mysql_connect ("meckpsych.startlogicmysql.com", "psyho", "psyho") or die ('I cannot connect to the database because: ' . mysql_error());
+
+mysql_select_db ("psyho");
+
+?>
