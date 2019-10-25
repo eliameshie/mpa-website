e9e0c7d29bd920887354cf1bca7bf8cb2e65fbd1
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-10-25 13:58:13 -0400

    Add files via upload

--- search.php
+++ search.php
@@ -1,26 +1,54 @@
 <?php 
-$link = mysqli_connect('meckpsych.startlogicmysql.com', 'psyho', 'mecklenburg'); 
+$link = mysql_connect('meckpsych.startlogicmysql.com', 'psyho', 'mecklenburg'); 
 if (!$link) { 
     die('Could not connect: ' . mysql_error()); 
 } 
 echo 'Connected successfully'; 
-mysqli_select_db(psyho); 
+mysql_select_db(psyho); 
 ?> 
 
 
-<?php 
-    $sql = "SELECT * FROM psychologist";
-    $result = mysqli_query($link, $sql);
-    $queryResults = mysqli_num_rows($result);
-
-    if ($queryResults > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<div> 
-                <h3>".$row['name']."</h3>
-                <h3>".$row['lasttName']."</h3>
-
-            </div";
+<?php
+    $query = $_GET['query']; 
+    // gets value sent over search form
+     
+    $min_length = 3;
+    // you can set minimum length of the query if you want
+     
+    if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
+         
+        $query = htmlspecialchars($query); 
+        // changes characters used in html to their equivalents, for example: < to &gt;
+         
+        $query = mysqli_real_escape_string($query);
+        // makes sure nobody uses SQL injection
+         
+        $raw_results = mysqli_query("SELECT * FROM psychologist
+            WHERE (`name` LIKE '%".$query."%') OR (`lastName` LIKE '%".$query."%')") or die(mysqli_error());
+             
+        // * means that it selects all fields, you can also write: `id`, `title`, `text`
+        // articles is the name of our table
+         
+        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+         
+        if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
+             
+            while($results = mysqli_fetch_array($raw_results)){
+            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+             
+                echo "<p><h3>".$results['name']."</h3>".$results['lastName']."</p>";
+                // posts results gotten from database(title and text) you can also show id ($results['id'])
+            }
+             
         }
+        else{ // if there is no matching rows do following
+            echo "No results";
+        }
+         
     }
-
+    else{ // if query length is less than minimum
+        echo "Minimum length is ".$min_length;
+    }
 ?>
