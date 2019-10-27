7b71fb25bf92803ec36f790a840de9347fe22155
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-10-27 15:38:22 -0400

    Add files via upload

--- contactform.php
+++ contactform.php
@@ -1,22 +1,75 @@
 <?php
-    $name = $_POST['text'];
-    $email = $_POST['email'];
-    $phone = $_POST['tel']
-    $message = $_POST['message'];
-    $from = 'From: MPACharlotte'; 
-    $to = 'eli@thenerodev.com'; 
-    $subject = 'Contacting MPACharlotte';
-    $human = $_POST['human'];
-			
-    $body = "From: $name\n E-Mail: $email\n Telephone: $phone\n Message:\n $message";
-				
-    if ($_POST['submit'] && $human == '4') {				 
-        if (mail ($to, $subject, $body, $from)) { 
-	    echo '<p>Your message has been sent!</p>';
-	} else { 
-	    echo '<p>Something went wrong, go back and try again!</p>'; 
-	} 
-    } else if ($_POST['submit'] && $human != '4') {
-	echo '<p>You answered the anti-spam question incorrectly!</p>';
-    }
+
+// SETTING THE EMAIL PORTION 
+
+if(isset($_POST['email'])) {
+    $emailTo = "eli@thenerodev.com"; 
+    $emailSubject = "Contacting MPA Charlotte";
+
+// IF THERE'S AN ERROR WITH THE FORM; DISPLAYS ERROR
+
+function formDie($error) {
+    echo "Sorry, but you've ran into a problem when entering information for this form."
+    echo "<br> </br";
+    echo "Error Occurred: ".$error;
+    die();
+}
+
+// VALIDATING THE FORM INFORMATION 
+if(!isset($_POST['text']) || 
+    !isset($_POST['email']) || 
+    !isset($_POST['message'])) {
+    formDie('Form does not have all the required materials to send to contact.');
+}
+
+// REQUIRED VARIABLES FOR THE FORM
+
+$name = $_POST['text'];
+$emailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+$errorMessage = "";
+$emailMessage = "Form details below. \n\n";
+$emailExp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/'; // EXPECETED EMAIL VALUES 
+$stringExp = "/^[A-Za-z .'-]+$/"; // EXPECTED STRING VALUES
+ 
+// CHECKING IF EXPECTED VALUES ARE SET
+if(!preg_match($emailExp, $emailFrom)) {
+    $errorMessage .= 'The Email Address entered contains invalid strings. Please Try Again.';
+}
+
+if(!preg_match($stringExp, $name)) {
+    $errorMessage .= 'The Name entered contains invalid strings. Please Try Again.';
+}
+
+if(strlen($message) < 2) {
+    $errorMessage .= "The Message doesn't have enough characters to send.";
+}
+
+if(strlen($errorMessage) > 0 ) {
+    formDie($errorMessage);
+}
+// CLEANS UP EMAIL IF THERE'S EXTRA SPACE OR BAD STRING TYPES
+function cleanString($string) {
+    $badString = array("content-type", "bcc:", "to:", "cc:", "href");
+    return str_replace($badString, "",$string);
+}
+
+$emailMessage .= "Name: ".clean_string($name)."\n";
+$emailMessage .= "Email: ".clean_string($emailFrom)."\n";
+
+$emailMessage .= "Message: ".clean_string($message)."\n";
+
+// CREATES THE EMAIL HEADERS
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+ 
+
+}
 ?>
