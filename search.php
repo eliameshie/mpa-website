8303748b2412923f48c191a7977340624eae6e13
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-10-29 20:59:56 -0400

    Add files via upload

--- search.php
+++ search.php
@@ -2,13 +2,33 @@
 
     if (isset($_POST['submit'])) {
         $conn = new mysqli("meckpsych.startlogicmysql.com","psyho","mecklenburg","psyho");
-        $q = $connection->real_escape_string($_POST['q']);
-    }
-        $data = $connection->query("SELECT * FROM psychologist");
-        if ($data->num_rows > 0 ) {
+        $search = mysqli_real_escape_string($conn, $_POST['search']);
+        $sql = "SELECT * FROM psychologist WHERE name LIKE  OR lastName like '%$search%' or company LIKE '%$search%' or address LIKE '%$search%' or city LIKE '%$search%' or state LIKE '%$search%' or zip LIKE '%$search%' or phone LIKE '%$search%' or otherphone LIKE '%$search%' or fax LIKE '%$search%' or email LIKE '%$search%'";
+        $result = mysqli_query($conn, $sql);
+        $queryResult = mysqli_num_rows($result);
 
+        echo "There are ".$queryResult;
+
+        if (queryResult > 0)  {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo <p>".$row['name']."</p> 
+                <p>".$row['lastName']."</p>
+                <p>".$row['company']."</p>
+                <p>".$row['address']."</p>
+                <p>".$row['city']."</p>
+                <p>".$row['state']."</p>
+                <p>".$row['zip']."</p>
+                <p>".$row['phone']."</p>
+                <p>".$row['otherphone']."</p>
+                <p>".$row['fax']."</p>
+                <p>".$row['email']."</p>;
+            }
         } else {
-            echo "Your search could not be found";
+            echo "There are no results that match your search!";
+        }
+ 
         }
 
 ?>
+
+ 
