576cdeaba6b603a460bb3e1650be50c06a14c51e
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-11-03 14:06:49 -0500

    Add files via upload

--- search.php
+++ search.php
@@ -6,7 +6,7 @@ $search = $_GET['search'];
 $search = $connection -> real_escape_string($search);
 
 $query = "SELECT * FROM psychologist WHERE name LIKE  OR lastName like '%$search%' or company LIKE '%$search%' or address LIKE '%$search%' or city LIKE '%$search%' or state LIKE '%$search%' or zip LIKE '%$search%' or phone LIKE '%$search%' or otherphone LIKE '%$search%' or fax LIKE '%$search%' or email LIKE '%$search%'";
-$result= $mysqli -> query($query);
+$result= $connection -> query($query);
 
 while($row = $result -> fetch_object()){
     echo "<div id='link' onClick='addText(\"".$row -> name."\");'>" . $row -> name . "</div>";  
