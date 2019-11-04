bd016a8e8b98414a634023080ded9c11ffc1b85a
eliameshie <53415046+eliameshie@users.noreply.github.com>
2019-11-03 19:42:22 -0500

    Add files via upload

--- search.php
+++ search.php
@@ -1,14 +1,34 @@
-<?php
+<?php 
 
-$connection = new mysqli('meckpsych.startlogicmysql.com', 'psyho', 'mecklenburg', 'psyho');
+$servername = 'meckpsych.startlogicmysql.com';
+$username = 'nerodev';
+$password = 'nero';
+$database = 'psyho';
 
-$search = $_GET['search'];
-$search = $connection -> real_escape_string($search);
+$conn = mysqli_connect($servername, $username, $password, $database) or die();
 
-$query = "SELECT * FROM psychologist WHERE name LIKE  OR lastName like '%$search%' or company LIKE '%$search%' or address LIKE '%$search%' or city LIKE '%$search%' or state LIKE '%$search%' or zip LIKE '%$search%' or phone LIKE '%$search%' or otherphone LIKE '%$search%' or fax LIKE '%$search%' or email LIKE '%$search%'";
-$result= $connection -> query($query);
+if (isset($_POST['submit'])) {
+    $keyword = $_POST['search']
+    $sql = "SELECT * FROM psychologist WHERE name LIKE '%$keyword%'";
+    $query = mysqli_query($conn, $sql);
 
-while($row = $result -> fetch_object()){
-    echo "<div id='link' onClick='addText(\"".$row -> name."\");'>" . $row -> name . "</div>";  
+
+    if (mysqli_num_rows($query) < 1) {
+        echo "There's not results that match your search.";
+
+    } else {
+        while($fetch = mysqli_fetch_assoc($query)) {
+            $name = $fetch['name'];
+            $lastname = $fetch['lastName'];
+
+            echo $name." - ".$lastName."<br>";
+        }
+
+
 }
-  ?>
+
+
+}
+
+
+?>
