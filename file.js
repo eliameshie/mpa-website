7b534be9cdf11d4a71058da82364821217103cc7
Clubin1 <53415042+Clubin1@users.noreply.github.com>
2020-03-05 18:41:05 -0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,32 @@
+document.getElementById("paypal").style.display = "none";
+
+var _validFileExtensions = [".pdf"];    
+function Validate(oForm) {
+    var arrInputs = oForm.getElementsByTagName("input");
+    for (var i = 0; i < arrInputs.length; i++) {
+        var oInput = arrInputs[i];
+        if (oInput.type == "file") {
+            var sFileName = oInput.value;
+            if (sFileName.length > 0) {
+                var blnValid = false;
+                for (var j = 0; j < _validFileExtensions.length; j++) {
+                    var sCurExtension = _validFileExtensions[j];
+                    if (sFileName.substr(sFileName.length - sCurExtension.length, sCurExtension.length).toLowerCase() == sCurExtension.toLowerCase()) {
+                        blnValid = true;
+                        break;
+                    }
+                }
+                
+                if (!blnValid) {
+                    alert("Please upload a pdf file.");
+                    return false;
+                } else {
+                    //show paypal button
+                    document.getElementById("paypal").style.display = "block";
+                }
+            }
+        }
+    }
+  
+    return true;
+}
